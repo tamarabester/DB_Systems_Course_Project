da7ccaf44d532fe248eeff84874ad9702eb35dce
pinj12 <68058383+pinj12@users.noreply.github.com>
2020-12-24 19:14:43 +0200

    Update movie_ratings.py

--- SRC/APPLICATION-SOURCE-CODE/models/movie_ratings.py
+++ SRC/APPLICATION-SOURCE-CODE/models/movie_ratings.py
@@ -1,10 +1,10 @@
 
 
 def get_ratings_with_comments():
-    query = "SELECT title, normalized_rating, comment" \
-            "FROM movies, movie_ratings" \
-            "WHERE movies.id = movie_ratings.movie_id" \
-            "AND movie_ratings.rating_source = 'users' LIMIT 500"
+    query = "SELECT title, normalized_rating, comment \
+            FROM movies, movie_ratings \
+            WHERE movies.id = movie_ratings.movie_id \
+            AND movie_ratings.rating_source = 'users' LIMIT 500"
 
     db_cursor = CONNECTION.cursor()
     db_cursor.execute(query)
@@ -24,12 +24,12 @@ def get_ratings_with_comments():
 
 def get_top_n_from_source(n, source):
     # TODO return list of {id, rating}
-    query = "SELECT title, movie_id, normalized_rating" \
-            "FROM movie_ratings, movies" \
-            "WHERE movies.id = movie_ratings.movie_id" \
-            "AND rating_source = %(source)s" \
-            "ORDER BY normalized_rating DESC" \
-            "LIMIT %(limit)s"
+    query = "SELECT title, movie_id, normalized_rating \
+            FROM movie_ratings, movies \
+            WHERE movies.id = movie_ratings.movie_id \
+            AND rating_source = %(source)s \
+            ORDER BY normalized_rating DESC \
+            LIMIT %(limit)s"
 
     db_cursor = CONNECTION.cursor()
     db_cursor.execute(query, dict(limit=n, source=source))
@@ -48,10 +48,10 @@ def get_top_n_from_source(n, source):
 
 
 def get_average_ratings_for_movie_id(movie_id):
-    query = "SELECT rating_source, AVG(normalized_rating)" \
-            "FROM movie_ratings" \
-            "WHERE movie_id = %(movie_id)s" \
-            "GROUP BY rating_source"
+    query = "SELECT rating_source, AVG(normalized_rating) \
+            FROM movie_ratings \
+            WHERE movie_id = %(movie_id)s \
+            GROUP BY rating_source"
 
     db_cursor = CONNECTION.cursor()
     db_cursor.execute(query, dict(movie_id=movie_id))
@@ -65,4 +65,18 @@ def get_average_ratings_for_movie_id(movie_id):
 
 
 def get_n_comments_for_movie_id(movie_id, n):
-    pass
+    query = "SELECT comment \
+    FROM movie_ratings \
+    WHERE movie_id = %(movie_id)s \
+    LIMIT %(limit)s"
+    
+    db_cursor = CONNECTION.cursor()
+    db_cursor.execute(query, dict(movie_id=movie_id, limit=n))
+    
+    comments = []
+    
+    for comment in db_cursor:
+        comments.append(comment)
+    
+    db_cursor.close()
+    retrun comments
