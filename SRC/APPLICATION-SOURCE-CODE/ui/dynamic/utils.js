fab6447391ecc9375df92b5597d97fee4cc08be3
edandavidai <75982389+edandavidai@users.noreply.github.com>
2021-01-13 21:39:15 +0200

    Update utils.js

--- SRC/APPLICATION-SOURCE-CODE/ui/dynamic/utils.js
+++ SRC/APPLICATION-SOURCE-CODE/ui/dynamic/utils.js
@@ -1,3 +1,121 @@
+
+function autocomplete(inp) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, j, val = this.value;
+      var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+               var arr = JSON.parse(this.responseText);
+          /*close any already open lists of autocompleted values*/
+          closeAllLists();
+          if (!val) { return false;}
+          currentFocus = -1;
+          /*create a DIV element that will contain the items (values):*/
+          a = document.createElement("DIV");
+          a.setAttribute("id", this.id + "autocomplete-list");
+          a.setAttribute("class", "autocomplete-items");
+          /*append the DIV element as a child of the autocomplete container:*/
+          document.getElementById("searchbar").parentNode.appendChild(a);
+          /*for each item in the array...*/
+          for (i = 0; i < arr.length; i++) {
+            /*check if the item starts with the same letters as the text field value:*/
+            for (j = 0; j < arr[i].length - val.length; j++){
+              if (arr[i].substr(j, val.length).toUpperCase() == val.toUpperCase()) {
+              /*create a DIV element for each matching element:*/
+                  /*console.log(arr[i])*/
+                  b = document.createElement("DIV");
+
+              /*make the matching letters bold:*/
+                  b.innerHTML = arr[i].substr(0, j);
+                  b.innerHTML += "<strong>" + arr[i].substr(j, val.length) + "</strong>";
+                  b.innerHTML += arr[i].substr(val.length + j);
+              /*insert a input field that will hold the current array item's value:*/
+                  b.innerHTML += "<input type='hidden' value='" + arr[i] + "' + padding-left:20px'>";
+
+                  /*b.getElementsByTagName('input').setAttribute("type", "");
+                  b.getElementsByTagName('input').setAttribute("style", "padding-left:20px")*/
+              /*execute a function when someone clicks on the item value (DIV element):*/
+                  b.addEventListener("click", function(e) {
+                  /*insert the value for the autocomplete text field:*/
+                  inp.value = this.getElementsByTagName("input")[0].value;
+                  /*close the list of autocompleted values,
+                  (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                  });
+                  a.appendChild(b);
+                  break;
+              }
+
+            }
+        
+      }
+      }
+    };
+    xhttp.open("GET", "/movie_name?text="+document.getElementById("searchbar").value, true);
+    xhttp.send();
+    
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
 /**
  * insets a child element to the html and returns that element.
  * for example:
@@ -48,12 +166,26 @@ function userTypeInTopSearchbar(e) {
     //console.log(e.target.value);
 }
 
-function searchMovieInDB(e){
-    // TODO! replace with movie search functionallity
-    if (e.key === 'Enter') {
-      console.log("Enter key has been pressed!")
+function SearchMovie(){
+
+  var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var movies = JSON.parse(this.responseText);
+            if(movies.length == 0){
+              alert("not a valid movie!")
+              return
+            }
+            window.location.replace("/movie?movie=" + movies[0].toString());
+
     }
+    };
+  xhttp.open("GET", "/id_for_title/"+document.getElementById("searchbar").value, true);
+  xhttp.send();
+
 }
+
 
  
 function init_navbar(link_list) {
@@ -70,24 +202,34 @@ function init_navbar(link_list) {
     
   
     // inset search bar
-    let searchcontainer, searchbar;
+    let searchcontainer, searchbar, form, button;
+    form = document.createElement('form')
+    form.setAttribute("autocomplete", "off")
+    form.setAttribute("action", "/action_page.php")
     searchcontainer = document.createElement('div')
-    searchcontainer.setAttribute("class", "searchcontainer")
-    parentelem.appendChild(searchcontainer)
+    searchcontainer.setAttribute("class", "autocomplete")
+    parentelem.appendChild(form)
+    form.appendChild(searchcontainer)
   
     searchbar = document.createElement('input')
     searchbar.setAttribute("type","text")
-    searchbar.setAttribute("style","width:15%")
+    searchbar.setAttribute("style","width:50%;")
     searchbar.setAttribute("name", "searchbar")
-    searchbar.setAttribute("class","right")
     searchbar.setAttribute("id","searchbar")
     searchbar.setAttribute("list","suggestions")
     searchbar.setAttribute("placeholder","Search a film..")
     searchcontainer.appendChild(searchbar)
+
+    button = document.createElement('a')
+    button.setAttribute("class", "btn")
+    button.setAttribute("type", "submit")
+    button.setAttribute("onclick", "SearchMovie()")
+    button.innerHTML = "Search"
+
+    parentelem.appendChild(button)
   
-    topinput = document.querySelector('#searchbar');
-    topinput.addEventListener('input', userTypeInTopSearchbar);
-    document.querySelector('#searchbar').addEventListener('keypress', searchMovieInDB);
+    autocomplete(searchbar)
+    button.addEventListener("click", SearchMovie)
   
 }
   
