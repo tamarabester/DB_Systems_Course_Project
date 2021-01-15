dca993619208871f72741e9c8b04888da77087d6
tamarabest94 <tamarabest94@gmail.com>
2021-01-15 10:24:17 +0200

    changing images for top rated

--- SRC/APPLICATION-SOURCE-CODE/ui/dynamic/TopRating.js
+++ SRC/APPLICATION-SOURCE-CODE/ui/dynamic/TopRating.js
@@ -1,44 +1,20 @@
 import {init_navbar, append_to_html_with_atterbutes} from './utils.js'
-import {links} from "./constants.js"
+import {links, img1,img2,img3} from "./constants.js"
 import {append_row_to_table, append_header_row_to_table} from "./TopPagesUtil.js"
-
-
-// function init_toppage(rating_type, n){
-//     var table_attributes, i, row_text;
-
-//     table_attributes = {"style":"width:100%", "id": "ratingtable"}
-//     append_to_html_with_atterbutes("ratingtablecontainer","table","ratingtable","",table_attributes)
-    
-    // append_header_row_to_table("ratingtable","toprow","ratingtabletop",["Rank","Title","Rating"])  
-
-    // var xhttp = new XMLHttpRequest();
-    // xhttp.onreadystatechange = function() {
-    //   if (this.readyState == 4 && this.status == 200) {
-    //     console.log(this.responseText);
-    //     var tops = JSON.parse(this.responseText);
-    //     for (i in tops){
-    //         var film = tops[i];
-    //         var title_link_text = film.title
-    //         row_text = [film.rank , title_link_text, film.rating]
-    //         append_row_to_table("ratingtable","row"+i,"",row_text,"td")
-    //     }
-    //   }
-    // };
-//     // IMDB, RT, USER
-//     xhttp.open("GET", "/top_rated/IMDB?n="+n, true);
-//     //xhttp.open("GET", "/imdb_rated?n="+n, true);
-//     xhttp.send();
-  
-// }
-
 
 function loadRatingView(source, n){
   const ids = ["ratingtablecontainer_USER","ratingtablecontainer_RT","ratingtablecontainer_IMDB"];
   const cur_id = "ratingtablecontainer_"+source;
-  var i;
-  //const imggrigs = [imggrig1,imggrig2,imggrig3];
-  //const random = Math.floor(Math.random() * 3);
-  //document.getElementById("grid1").innerHTML = imggrigs[random]
+
+  if (source=="USER"){
+    document.getElementById("imggrid").innerHTML = img1;
+  }
+  if (source=="IMDB"){
+    document.getElementById("imggrid").innerHTML = img2;
+  }
+  if (source=="RT"){
+    document.getElementById("imggrid").innerHTML = img3;
+  }
 
   for(i in ids){
     document.getElementById(ids[i]).innerHTML="";
@@ -73,6 +49,7 @@ function loadRatingTable(id, source, n){
 
 }
 
-
+/* on page load */
 init_navbar(links);
+document.getElementById("imggrid").innerHTML = img1;
 window.loadRatingView = loadRatingView;
